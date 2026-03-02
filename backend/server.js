5dce24edbed46fdaba9642e27852176adbf33689
Vignesh RM <vignesh2210433@ssn.edu.in>
2026-03-02 02:20:08 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,5 +24,5 @@ app.get('*', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
