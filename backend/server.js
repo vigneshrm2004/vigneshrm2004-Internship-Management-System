653cf75e70f1e24e03c0137d7a87b8e0e98ed402
Vignesh RM <vignesh2210433@ssn.edu.in>
2026-03-02 02:21:59 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 // ------------------------------
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
